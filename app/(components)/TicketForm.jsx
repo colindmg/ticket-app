f20c83fd7bd7f950d23be66f0244c22575847791
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-12 17:48:55 -0400

    Possibilité de modification d'un ticket

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -3,10 +3,12 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-const TicketForm = () => {
+const TicketForm = ({ ticket }) => {
+  const EDITMODE = ticket._id === "new" ? false : true;
+
   const router = useRouter();
 
-  const startingTicketDate = {
+  const startingTicketData = {
     title: "",
     description: "",
     priority: 1,
@@ -15,7 +17,16 @@ const TicketForm = () => {
     category: "Hardware Problem",
   };
 
-  const [formData, setFormData] = useState(startingTicketDate);
+  if (EDITMODE) {
+    startingTicketData.title = ticket.title;
+    startingTicketData.description = ticket.description;
+    startingTicketData.priority = ticket.priority;
+    startingTicketData.progress = ticket.progress;
+    startingTicketData.status = ticket.status;
+    startingTicketData.category = ticket.category;
+  }
+
+  const [formData, setFormData] = useState(startingTicketData);
 
   const handleChange = (e) => {
     const value = e.target.value;
@@ -29,18 +40,32 @@ const TicketForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("/api/Tickets", {
-      method: "POST",
-      body: JSON.stringify({ formData }),
-      "content-type": "application/json",
-    });
 
-    if (!res.ok) {
-      throw new Error("Failed to create ticket");
+    if (EDITMODE) {
+      const res = await fetch(`/api/Tickets/${ticket._id}`, {
+        method: "PUT",
+        body: JSON.stringify({ formData }),
+        "content-type": "application/json",
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to update ticket");
+      }
+    } else {
+      const res = await fetch("/api/Tickets", {
+        method: "POST",
+        body: JSON.stringify({ formData }),
+        "content-type": "application/json",
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to create ticket");
+      }
     }
 
     router.refresh();
     router.push("/");
+    router.refresh();
   };
 
   return (
@@ -50,7 +75,7 @@ const TicketForm = () => {
         method="POST"
         onSubmit={handleSubmit}
       >
-        <h3>Create your ticket</h3>
+        <h3>{EDITMODE ? "Update your ticket" : "Create your ticket"}</h3>
 
         <label>Title</label>
         <input
@@ -150,7 +175,11 @@ const TicketForm = () => {
           <option value="done">Done</option>
         </select>
 
-        <input type="submit" className="btn" value="Create Ticket" />
+        <input
+          type="submit"
+          className="btn"
+          value={EDITMODE ? "Update your ticket" : "Create your ticket"}
+        />
       </form>
     </div>
   );
